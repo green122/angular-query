ac45dd617920664f884caa393d34618a2ba1017c
Andrei Gerasimov <andrei.gerasimov@productsup.com>
2021-07-10 17:08:49 +0300

    basic implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {
+  HttpQueryService,
+  QueryResult,
+  QueryState,
+} from './http-query.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lib-query';
+  query$: Observable<QueryState<{ name: string }>>;
+
+  trigger$ = new Subject<string>();
+
+  fetch: (id?: string) => void;
+
+  constructor(
+    private httpQuery: HttpQueryService,
+    private httpClient: HttpClient
+  ) {
+    const createdPeopleQuery = this.httpQuery.createQuery({
+      key: 'starwarspeople',
+      query: (id?: string) =>
+        this.httpClient.get<{ name: string }>(
+          `https://swapi.dev/api/people/${id}`
+        ),
+      trigger: this.trigger$,
+    });
+
+    const createdShipQuery = this.httpQuery.createQuery({
+      key: 'starwarspeople',
+      query: (arg?: { name: string }) => {
+        console.log(arg);
+        return this.httpClient.get<{ name: string }>(
+          `https://swapi.dev/api/ships`
+        );
+      },
+      trigger: createdPeopleQuery.data$,
+    });
+    this.query$ = createdPeopleQuery.state$;
+    this.fetch = createdPeopleQuery.fetch;
+
+    createdShipQuery.state$.subscribe(console.log);
+  }
+
+  ngOnInit() {
+    // this.query$.subscribe(console.log);
+    this.fetch('1');
+
+    setTimeout(() => this.trigger$.next('3'), 3000);
+  }
+}
---
+++ src/app/http-query.service.ts
@@ -0,0 +1,125 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subject, throwError } from 'rxjs';
+import {
+  catchError,
+  filter,
+  map,
+  startWith,
+  switchMap,
+  switchMapTo,
+  tap,
+} from 'rxjs/operators';
+
+type Query<TResponseData, TDependency> =
+  | ((dep?: TDependency) => Observable<TResponseData>)
+  | Observable<TResponseData>;
+
+interface QueryArgs<TResponseData, TDependency> {
+  key: string;
+  query: Query<TResponseData, TDependency>;
+  defaultData?: TResponseData;
+  trigger?: Observable<TDependency>;
+}
+
+type ErrorStatus = { loading: false; data: null; error: string };
+
+export type QueryState<T> =
+  | { loading: true; data: null; error: null }
+  | { loading: false; data?: T; error: null }
+  | ErrorStatus;
+
+export interface QueryResult<TResponseData, TDependency = unknown> {
+  state$: Observable<QueryState<TResponseData>>;
+  data$: Observable<TResponseData>;
+  fetch(arg?: TDependency): void;
+}
+
+type FunctionType = (...args: any) => any;
+const isFunction = (arg: unknown): arg is FunctionType => {
+  return typeof arg === 'function';
+};
+
+const notIsNull = <T>(arg: T | null): arg is T => {
+  return arg !== null;
+};
+
+const isDefined = <T>(arg: T | undefined): arg is T => {
+  return typeof arg !== 'undefined';
+};
+
+const isPropertyDefined =
+  <T, K extends keyof T>(prop: K) =>
+  (arg: T): arg is T => {
+    return isDefined(arg[prop]);
+  };
+
+const getRequest$ = <TResponseData, TDependency>(
+  query: Query<TResponseData, TDependency>,
+  dep?: TDependency
+) => {
+  const request$ = isFunction(query) ? query(dep) : query;
+  return request$.pipe(
+    map<TResponseData, QueryState<TResponseData>>((response) => ({
+      loading: false,
+      data: response,
+      error: null,
+    })),
+    startWith<QueryState<TResponseData>>({
+      loading: true,
+      error: null,
+      data: null,
+    })
+  );
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpQueryService {
+  constructor() {}
+
+  createQuery<TResponseData, TDependency>(
+    args: QueryArgs<TResponseData, TDependency>
+  ): QueryResult<TResponseData, TDependency> {
+    const subject$ = new BehaviorSubject<
+      { variable?: TDependency } | undefined
+    >(undefined);
+
+    const getQuerySubject = (
+      trigger: Observable<{ variable?: TDependency } | undefined>
+    ) => {
+      return trigger.pipe(
+        filter(isDefined),
+        switchMap(({ variable }) => getRequest$(args.query, variable)),
+        catchError((error) => {
+          return of<ErrorStatus>({
+            loading: false,
+            data: null,
+            error: (error.message as string) || 'something wrong',
+          });
+        })
+      );
+    };
+
+    const query$ = args.trigger
+      ? getQuerySubject(args.trigger.pipe(map((v) => ({ variable: v }))))
+      : getQuerySubject(subject$);
+
+    if (!isFunction(args.query)) {
+      subject$.next(undefined);
+    }
+
+    return {
+      state$: query$,
+      data$: query$.pipe(
+        map((state) => state.data),
+        tap((v) => v),
+        filter(isDefined),
+        filter(notIsNull)
+      ),
+      fetch(dep?: TDependency) {
+        subject$.next({ variable: dep });
+      },
+    };
+  }
+}
